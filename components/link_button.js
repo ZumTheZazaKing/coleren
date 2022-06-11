393ff67032b98b47ee5bd07ea523b35f7321b1d3
ZumTheZazaKing <zahidi85543@gmail.com>
2022-06-11 17:22:29 +0800

    editor abnormal can't fix

--- components/link_button.js
+++ components/link_button.js
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
-export default function LinkButton({title, link, custom, query}){
+export default function LinkButton({title, link, custom, query, newTab}){
   return(
     <div className={`m-2 rounded-xl shadow-lg overflow-hidden flex flex-col ${custom}`}>
       <Link 
@@ -10,7 +10,7 @@ export default function LinkButton({title, link, custom, query}){
         }}
         as={`/${link}`}
       >
-        <a>
+        <a target={newTab ? "_blank" : ""}>
           <h1 className='p-4 font-semibold'>{title}</h1>
         </a>
       </Link>
--- pages/editors/html_editor.js
+++ pages/editors/html_editor.js
@@ -1,4 +1,5 @@
 import { useRouter } from 'next/router';
+import Head from 'next/head';
 import { useState } from 'react';
 import Editor from 'react-simple-code-editor';
 import { highlight, languages } from 'prismjs/components/prism-core';
@@ -10,20 +11,37 @@ import 'prismjs/themes/prism.css'; //Example style, you can use another
 export default function HtmlEditor(){
 
     const router = useRouter();
-    const [code, setCode] = useState(JSON.stringify(router.query.code).replace(/["\[\]:]/g,"").replace(/,/g,'\n') || "");
+    const [code, setCode] = useState(router.query.code ? JSON.stringify(router.query.code).replace(/["\[\]:]/g,"").replace(/,/g,'\n') : "");
+    const [iframeCode, setIframeCode] = useState(`data:text/html;charset=utf-8,${encodeURI(code)}`)
+
+    const run = () => {
+        setIframeCode(`data:text/html;charset=utf-8,${encodeURI(code)}`);
+    }
 
     return (
-        <>
-        <Editor
-            value={code ? code : ""}
-            onValueChange={code => setCode(code)}
-            highlight={code => highlight(code, languages.html)}
-            padding={10}
-            style={{
-                fontFamily: '"Fira code", "Fira Mono", monospace',
-                fontSize: 16,
-            }}
-        />
-      </>
+        <div className='h-screen w-screen flex-col flex fixed top-0 left-0 bg-slate-500 sm:truncate'>
+            <Head><title>Coleren | HTML Editor</title></Head>
+            <div onClick={() => run()} className='p-4'>
+                <button className='bg-blue-500 text-white font-bold py-2 px-5 rounded-md shadow-md'>Run</button>
+            </div>
+           
+           <div className='flex-col sm:flex-row overflow-auto sm:overflow-hidden  grow'>
+                <Editor
+                    value={code ? code : ""}
+                    onValueChange={code => setCode(code)}
+                    highlight={code => highlight(code, languages.html)}
+                    padding={10}
+                    style={{
+                        fontFamily: '"Fira code", "Fira Mono", monospace',
+                        fontSize: 16,
+                        outline: 'none',
+                    }}
+                    className="bg-slate-200 w-full sm:w-2/4  h-full overflow-auto"
+                />
+                <div className='w-full sm:w-2/4 h-full bg-white overflow-auto'>
+                    <iframe src={iframeCode} title="output"/>
+                </div>
+           </div>
+        </div>
     )
 }
