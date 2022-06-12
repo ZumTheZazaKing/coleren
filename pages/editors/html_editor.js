2e24d45ef70281b7e81335bb0113a481d7782896
ZumTheZazaKing <zahidi85543@gmail.com>
2022-06-12 14:36:06 +0800

    Editor finished. Time to put tutorials or configure accounts

--- pages/editors/html_editor.js
+++ pages/editors/html_editor.js
@@ -11,8 +11,8 @@ import 'prismjs/themes/prism.css'; //Example style, you can use another
 export default function HtmlEditor(){
 
     const router = useRouter();
-    const [code, setCode] = useState(router.query.code ? JSON.stringify(router.query.code).replace(/["\[\]:]/g,"").replace(/,/g,'\n') : "");
-    const [iframeCode, setIframeCode] = useState(`data:text/html;charset=utf-8,${encodeURI(code)}`)
+    const [code, setCode] = useState(router.query.code ? JSON.stringify(router.query.code).replace(/["\[\]:]/g,"").replace(/,/g,'\n') : "Write your code here...");
+    const [iframeCode, setIframeCode] = useState(router.query.code ? `data:text/html;charset=utf-8,${encodeURI(code)}` : `data:text/html;charset=utf-8,${encodeURI("This is where the output will be")}`)
 
     const run = () => {
         setIframeCode(`data:text/html;charset=utf-8,${encodeURI(code)}`);
@@ -21,26 +21,24 @@ export default function HtmlEditor(){
     return (
         <div className='h-screen w-screen flex-col flex fixed top-0 left-0 bg-slate-500 sm:truncate'>
             <Head><title>Coleren | HTML Editor</title></Head>
-            <div onClick={() => run()} className='p-4'>
+            <div onClick={run} className='p-4'>
                 <button className='bg-blue-500 text-white font-bold py-2 px-5 rounded-md shadow-md'>Run</button>
             </div>
            
-           <div className='flex-col sm:flex-row overflow-auto sm:overflow-hidden  grow'>
-                <Editor
-                    value={code ? code : ""}
-                    onValueChange={code => setCode(code)}
-                    highlight={code => highlight(code, languages.html)}
-                    padding={10}
-                    style={{
-                        fontFamily: '"Fira code", "Fira Mono", monospace',
-                        fontSize: 16,
-                        outline: 'none',
-                    }}
-                    className="bg-slate-200 w-full sm:w-2/4  h-full overflow-auto"
-                />
-                <div className='w-full sm:w-2/4 h-full bg-white overflow-auto'>
-                    <iframe src={iframeCode} title="output"/>
-                </div>
+           <div className='flex flex-col sm:flex-row overflow-auto sm:overflow-hidden  grow'>
+                <div className="bg-slate-200 w-full sm:w-2/4 h-screen sm:h-full overflow-auto">
+                    <Editor
+                        value={code}
+                        onValueChange={code => setCode(code)}
+                        highlight={code => highlight(code, languages.html)}
+                        padding={10}
+                        style={{
+                            fontFamily: '"Fira code", "Fira Mono", monospace',
+                            fontSize: 16,
+                        }}
+                    />
+                    </div>
+                <iframe src={iframeCode} title="output" className='w-full sm:w-2/4 h-screen sm:h-full bg-white overflow-auto p-0'/>
            </div>
         </div>
     )
